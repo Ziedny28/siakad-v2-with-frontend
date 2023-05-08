f4f8d5d3bc4055bbae0b27f3ac1d45e76f4eb73d
Syaroful <syaroful10403@gmail.com>
2023-05-08 14:18:02 +0700

    mengemmbalikan laout yang dihapus

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Siakad Teacher Dashboard</title>
+    <link rel="stylesheet" href="assets/modules/bootstrap-5.1.3/css/bootstrap.css" />
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <!-- FontAwesome CSS-->
+    <link rel="stylesheet" href="assets/modules/fontawesome6.1.1/css/all.css" />
+    <!-- Boxicons CSS-->
+    <link rel="stylesheet" href="assets/modules/boxicons/css/boxicons.min.css" />
+    <!-- Apexcharts  CSS -->
+    <link rel="stylesheet" href="assets/modules/apexcharts/apexcharts.css" />
+</head>
+<body>
+    @yield('content')
+    <!-- bootstrap js -->
+    <script src="assets/modules/jquery/jquery.min.js"></script>
+    <script src="assets/modules/bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/modules/popper/popper.min.js"></script>
+
+    <!-- Chart Js -->
+    <script src="assets/modules/apexcharts/apexcharts.js"></script>
+    <script src="assets/js/ui-apexcharts.js"></script>
+
+    <!-- Template JS File -->
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
