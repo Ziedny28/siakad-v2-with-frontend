15b4f4ca0093dbe14723c20739492fabd28ef00b
Syaroful <syaroful10403@gmail.com>
2023-05-10 09:26:20 +0700

    halaman student

---
+++ resources/views/partials/student-sidebar.blade.php
@@ -0,0 +1,52 @@
+<!--Sidebar-->
+<div class="sidebar transition overlay-scrollbars animate__animated animate__slideInLeft">
+    <div class="sidebar-content">
+        <div id="sidebar">
+            <!-- Logo -->
+            <div class="logo">
+                <img src="assets/images/logo-man1.png" />
+                <h3>SIAKAD</h3>
+                <h5>MAN 1 Kota Malang</h5>
+            </div>
+
+            <ul class="side-menu">
+                <li>
+                    <a href="/dashboard-student" class="{{ request()->is('dashboard-student') ? 'active' : ''}}">
+                        <i class="bx bxs-dashboard icon"></i> Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bx bxs-user icon"></i> My Profile
+                    </a>
+                </li>
+
+                <!-- Divider-->
+                <li class="divider" data-text="STARTER">DATA NILAI</li>
+                <li>
+                    <a href="/student-task" class="{{ request()->is('task') ? 'active' : ''}}">
+                        <i class='bx bx-task icon'></i>
+                        Tugas
+                    </a>
+                </li>
+                <li>
+                    <a href="/student-score" class="{{ request()->is('task') ? 'active' : ''}}">
+                        <i class='bx bx-task icon'></i>
+                        Nilai
+                    </a>
+                </li>
+                <!-- Divider-->
+                <li class="divider" data-text="STARTER">LIHAT JADWAL</li>
+
+                <li>
+                    <a href="/student-schedule" class="{{ request()->is('schedule') ? 'active' : ''}}">
+                        <i class='bx bxs-time-five icon'></i>
+                        Jadwal Siswa
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="sidebar-overlay"></div>
+<!-- End Sidebar-->
