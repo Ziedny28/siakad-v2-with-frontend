28c5a1aa3b349d0de2a2cda6e23e004d477c69fd
Syaroful <syaroful10403@gmail.com>
2023-06-05 08:42:52 +0700

    delete command code

--- resources/views/student/score/index.blade.php
+++ resources/views/student/score/index.blade.php
@@ -84,22 +84,7 @@
                                         {{-- paginator --}}
                                         {{ $scores->links() }}
                                     </div>
-
-
                                 </div>
-                                {{-- <div class="pagination-bar">
-                                    <ul class="pagination pagination-success  justify-content-center">
-                                        <li class="page-item disabled">
-                                            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                                        </li>
-                                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#">Next</a>
-                                        </li>
-                                    </ul>
-                                </div> --}}
                             </div>
 
                         </div>
