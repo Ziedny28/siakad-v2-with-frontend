49b96c1717585cba45cb06def2324831ec4b2b29
Syaroful <syaroful10403@gmail.com>
2023-06-13 16:35:26 +0700

    membuat rata rata menjadi integer

--- app/Http/Controllers/StudentPageController.php
+++ app/Http/Controllers/StudentPageController.php
@@ -2,59 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
+use App\Models\ClassRoom;
 use App\Models\Score;
 use App\Models\Student;
-use App\Models\ClassRoom;
+use App\Models\Task;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Query\Builder;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class StudentPageController extends Controller
 {
     // menampilkan halaman tugas murid ini
-    function studentTask()
+    public function studentTask()
     {
         $task = Task::with('teacher.subject')->where('class_room_id', Auth::user()->class_room_id)
             ->orderBy('teacher_id')->paginate(10);
 
         // topboxes
-        $scoreAvg = Score::where('student_id', Auth::user()->id)->avg('score'); //getting average score of this student
+        $scoreAvg = round(Score::where('student_id', Auth::user()->id)->avg('score'), 1); //getting average score of this student
         $taskCount = Task::where('class_room_id', Auth::user()->class_room_id)->count();
 
         return view('student.task.index', ['tasks' => $task, 'taskCount' => $taskCount, 'scoreAvg' => $scoreAvg]);
     }
 
-
     // menampilkan halaman nilai murid ini
-    function studentScore()
+    public function studentScore()
     {
         $student_id = Auth::user()->id; //getting student id
         $score = Score::with('task.teacher.subject')->where('student_id', $student_id)->orderBy(function (Builder $q) {
             $q->select('teacher_id')->from('tasks')->whereColumn('task_id', 'tasks.id');
         })->paginate(10);
         // topboxes
-        $scoreAvg = Score::where('student_id', $student_id)->avg('score'); //getting average score of this student
+        $scoreAvg = round(Score::where('student_id', $student_id)->avg('score'), 1); //getting average score of this student
         $taskCount = Task::where('class_room_id', Auth::user()->class_room_id)->count();
 
         return view('student.score.index', ['scores' => $score, 'taskCount' => $taskCount, 'scoreAvg' => $scoreAvg]);
     }
 
-    function studentSchedule()
+    public function studentSchedule()
     {
         $class_room = ClassRoom::findOrFail(Auth::user()->class_room_id);
         return view('student.schedule.index', ['class_room' => $class_room]);
     }
 
-    function studentProfile()
+    public function studentProfile()
     {
         return view('student.my-profile', ['student' => Auth::user()]);
     }
 
-
-    function changePassword()
+    public function changePassword()
     {
         $student = Student::findOrFail(auth()->user()->id);
         return view('student.change-password', [
@@ -62,7 +60,7 @@ class StudentPageController extends Controller
         ]);
     }
 
-    function saveChangePassword(Request $request)
+    public function saveChangePassword(Request $request)
     {
         $credentials = $request->validate($this->credentialRules);
         //if student's old pass is not same with the one that just inputted
